757f44d8a90d2d0769f949489280528a3f36718e
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-11 11:33:51 +0530

    bug

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -92,7 +92,7 @@ const Login = () => {
 
   return (
     <div className="relative">
-      <div className="absolute bg-gradient-to-b from-black h-screen w-screen">
+      <div className="absolute bg-gradient-to-b from-black h-screen w-[100%]">
         <div className="w-64 h-12 px-4 pt-0">
           <img
             src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
