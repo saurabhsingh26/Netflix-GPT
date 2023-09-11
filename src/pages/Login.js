05b25e35b8760fcde2e5793aa96d12d753797ef1
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-11 12:27:27 +0530

    background image

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -91,8 +91,8 @@ const Login = () => {
   };
 
   return (
-    <div className="relative">
-      <div className="absolute bg-gradient-to-b from-black h-screen w-[100%]">
+    <div style={myStyle} className="flex justify-center">
+      <div className=" bg-gradient-to-b from-black h-[100vh] w-[100%]">
         <div className="w-64 h-12 px-4 pt-0">
           <img
             src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
@@ -100,7 +100,7 @@ const Login = () => {
           />
         </div>
       </div>
-      <div className="w-[35%] absolute bg-[#000000BF] top-[15%] left-[32.5%] flex justify-center ">
+      <div className="w-[100%] sm:w-[60%] md:w-[50%] lg:w-[35%] bg-[#000000BF] flex justify-center absolute top-32">
         <div className="w-[70%]">
           <form onSubmit={(e) => e.preventDefault()}>
             <h1 className="text-white mt-10 text-4xl font-semibold">
@@ -144,9 +144,9 @@ const Login = () => {
               {isSignInForm ? "Login" : "Sign Up"}
             </button>
           </form>
-          <div className="mt-5 mb-20">
+          <div className="mb-16">
             <p
-              className="text-base text-[#737373] mt-16 cursor-pointer"
+              className="text-base text-[#737373] mt-8 cursor-pointer"
               onClick={toggleSignInForm}
             >
               {isSignInForm ? (
@@ -161,19 +161,7 @@ const Login = () => {
                 </>
               )}
             </p>
-            <p className="text-[#8C8C8C] text-[13px] mt-2">
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.
-            </p>
           </div>
-        </div>
-      </div>
-      <div>
-        <div className="">
-          <img
-            src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-            alt="background"
-          />
         </div>
       </div>
     </div>
@@ -181,3 +169,11 @@ const Login = () => {
 };
 
 export default Login;
+
+const myStyle = {
+  backgroundImage:
+    "url('https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_small.jpg')",
+  height: "100vh",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
