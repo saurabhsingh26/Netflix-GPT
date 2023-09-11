b9c744b33eef2bd945a94ea2b43117ce80d61e66
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-11 16:16:17 +0530

    bug

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { MOVIE_OPTIONS } from "../utils/constants";
