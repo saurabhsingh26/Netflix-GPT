6e844c6310f6065441e4f8144eabdce68f0857e4
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-11 16:15:35 +0530

    setup movies slice

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { MOVIE_OPTIONS } from "../utils/constants";
 import Header from "../components/Header";
+import { addNowPlayingMovies } from "../Redux/slices/movieSlice";
 const Browse = () => {
-
+  
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', MOVIE_OPTIONS);
     const json = await data.json();
-    console.log("json", json);
+    console.log("json", json.results);
+    dispatch(addNowPlayingMovies(json.results));
   } 
 
   useEffect(() => {
