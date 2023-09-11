ac976ddf0f2ec396876109e850f5f469f6536945
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-11 20:00:37 +0530

    bug

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,7 +9,7 @@ const VideoBackground = ({ movieId }) => {
     <div className="z-50">
       <iframe
         className="w-[98.7vw] aspect-video"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=G8BbXUfNjICHxmXr&amp;controls=0&autoplay=1&loop=1`}
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=G8BbXUfNjICHxmXr&amp;controls=0&autoplay=1&loop=1&color=white`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
