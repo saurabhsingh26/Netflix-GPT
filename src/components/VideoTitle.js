64fd69a9a12c41a6c8d4469f00dd91b53461e4fd
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-12 13:47:37 +0530

    bug

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="py-4 sm:py-[70px] lg:py-[205px] px-2 sm:px-12 absolute top-0 bg-gradient-to-r from-black">
+    <div className="w-[98.7vw] aspect-video flex flex-col justify-center px-2 sm:px-12 absolute top-0 bg-gradient-to-r from-black">
       <h1 className="text-2xl lg:text-4xl lg:font-bold text-white">{title}</h1>
-      <p className="py-3 text-[14px] lg:text-lg text-gray-500 lg:text-white lg:w-1/4">{overview}</p>
+      <p className="py-3 text-[14px] lg:text-lg text-gray-500 lg:text-white lg:w-1/4">
+        {overview}
+      </p>
       <div>
         <button className="px-8 py-2 bg-white text-black rounded-md">
           ▶Play
