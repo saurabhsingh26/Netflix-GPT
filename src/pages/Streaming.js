37c097bc040037b316a48cf3e15b30c07b9a5f02
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-09-23 16:00:19 +0530

    page added

---
+++ src/pages/Streaming.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Streaming = () => {
+  return <div></div>;
+};
+
+export default Streaming;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,13 @@ import GptSearch from "./GptSearch";
 import Error from "./Error";
 import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestions from "./GptMovieSuggestions";
-export { Login, Browse, GptSearch, GptSearchBar, GptMovieSuggestions, Error };
+import Streaming from "./Streaming";
+export {
+  Login,
+  Browse,
+  GptSearch,
+  GptSearchBar,
+  GptMovieSuggestions,
+  Streaming,
+  Error,
+};
